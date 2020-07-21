4f384c60b1b1ac97786dfaf4f59afcd547d12fc5
Prabhav Parashar <prabhav.parashar@gmail.com>
2020-07-20 22:17:48 -0400

    added better error handling

--- cmd/main.go
+++ cmd/main.go
@@ -12,13 +12,23 @@ func main() {
 	var operation flags.Operation = flags.GetFlags()
 	switch operation.Name {
 	case "set":
-		var ip string = getddns.GetPublicIPv4Addr(operation.Parameters.APIkey, operation.Parameters.APIURL).ReturnMessage
-		response := setddns.SetHostnameIPv4Addr(operation.Parameters.Hostname, operation.Parameters.APIkey, operation.Parameters.SharedSecret, ip, operation.Parameters.APIURL)
-		log.Println(response.ReturnStatus)
-		log.Println(response.ReturnMessage)
+		getIPv4response, err := getddns.GetPublicIPv4Addr(operation.Parameters.APIkey, operation.Parameters.APIURL)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		var ip string = getIPv4response.ReturnMessage
+		setIPv4response, err := setddns.SetHostnameIPv4Addr(operation.Parameters.Hostname, operation.Parameters.APIkey, operation.Parameters.SharedSecret, ip, operation.Parameters.APIURL)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		log.Println(setIPv4response.ReturnStatus)
+		log.Println(setIPv4response.ReturnMessage)
 	case "get":
-		response := getddns.GetPublicIPv4Addr(operation.Parameters.APIkey, operation.Parameters.APIURL)
-		log.Println(response.ReturnStatus)
-		log.Println(response.ReturnMessage)
+		getIPv4response, err := getddns.GetPublicIPv4Addr(operation.Parameters.APIkey, operation.Parameters.APIURL)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		log.Println(getIPv4response.ReturnStatus)
+		log.Println(getIPv4response.ReturnMessage)
 	}
 }
