657a8f0c2f51257392be44d85c10e60181199c72
Prabhav Parashar <prabhav.parashar@gmail.com>
2020-07-20 22:13:59 -0400

    abstracted http request to API endpoint into common/ package

--- pkg/getddns/getddns.go
+++ pkg/getddns/getddns.go
@@ -1,48 +1,19 @@
 package getddns
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"log"
-	"net/http"
 	"strings"
+
+	common "../common"
 )
 
-type Response struct {
-	ReturnMessage string `json:"return_message"`
-	ReturnStatus  string `json:"return_status"`
-}
-
-func GetPublicIPv4Addr(apiKey string, apiURL string) *Response {
+// GetPublicIPv4Addr gets the public IPv4 address of the network the device calling the function is on
+func GetPublicIPv4Addr(apiKey string, apiURL string) (common.Response, error) {
 	if !strings.HasSuffix(apiURL, "/") {
 		apiURL = apiURL + "/"
 	}
 
 	constructedAPIEndpoint := fmt.Sprintf("%s?mode=get", apiURL)
 
-	APIclient := &http.Client{}
-	req, err := http.NewRequest("GET", constructedAPIEndpoint, nil)
-	if err != nil {
-		log.Fatalln(err, "Error creating get-ipv4-address request before the request could be sent out")
-	}
-	req.Header.Add("x-api-key", apiKey)
-
-	resp, err := APIclient.Do(req)
-	if err != nil {
-		log.Fatalln(err, "Error executing get-ipv4 request to DDNS API")
-	}
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatalln(err, "Error reading get-ipv4 response body from DDNS API")
-	}
-
-	response := &Response{}
-
-	jsonerr := json.Unmarshal([]byte(body), response)
-	if jsonerr != nil {
-		log.Fatalln(jsonerr)
-	}
-
-	return response
+	return common.SendHTTPRequest(constructedAPIEndpoint, apiKey)
 }
--- pkg/setddns/setddns.go
+++ pkg/setddns/setddns.go
@@ -3,23 +3,16 @@ package setddns
 import (
 	"crypto/sha256"
 	"encoding/hex"
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"log"
-	"net/http"
 	"strings"
+
+	common "../common"
 )
 
-type Response struct {
-	ReturnMessage string `json:"return_message"`
-	ReturnStatus  string `json:"return_status"`
-}
-
 // SetHostnameIPv4Addr set's the IPv4 Address for a particular hostname
-func SetHostnameIPv4Addr(hostname string, apiKey string, sharedSecret string, WANIPv4 string, apiURL string) *Response {
-	preSha256SumConcatenation := fmt.Sprintf("%s%s.%s", WANIPv4, hostname, sharedSecret)
-	sha256SumOfConcatenation := sha256.Sum256([]byte(preSha256SumConcatenation))
+func SetHostnameIPv4Addr(hostname string, apiKey string, sharedSecret string, WANIPv4 string, apiURL string) (common.Response, error) {
+	preSumCalcContenation := fmt.Sprintf("%s%s.%s", WANIPv4, hostname, sharedSecret)
+	sha256SumOfConcatenation := sha256.Sum256([]byte(preSumCalcContenation))
 	sha256SumAsString := hex.EncodeToString(sha256SumOfConcatenation[:])
 
 	if !strings.HasSuffix(apiURL, "/") {
@@ -28,28 +21,5 @@ func SetHostnameIPv4Addr(hostname string, apiKey string, sharedSecret string, WA
 
 	constructedAPIEndpoint := fmt.Sprintf("%s?mode=set&hostname=%s&hash=%s", apiURL, hostname, sha256SumAsString)
 
-	APIclient := &http.Client{}
-	req, err := http.NewRequest("GET", constructedAPIEndpoint, nil)
-	if err != nil {
-		log.Fatalln(err, "Error creating set-ipv4 request before the request could be sent out")
-	}
-	req.Header.Add("x-api-key", apiKey)
-
-	resp, err := APIclient.Do(req)
-	if err != nil {
-		log.Fatalln(err, "Error executing set-ipv4 request to DDNS API")
-	}
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatalln(err, "Error reading set-ipv4 response body from DDNS API")
-	}
-
-	response := &Response{}
-
-	jsonerr := json.Unmarshal([]byte(body), response)
-	if jsonerr != nil {
-		log.Fatalln(jsonerr)
-	}
-
-	return response
+	return common.SendHTTPRequest(constructedAPIEndpoint, apiKey)
 }
